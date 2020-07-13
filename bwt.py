cfaa98f258456546525f98d8c82aa9b9541a3558
Lorenzo Tabasso <lorenzo.tabasso96@gmail.com>
2020-07-13 16:02:17 +0200

    BWT ready for use. Enjoy!

--- bwt.py
+++ bwt.py
@@ -1,66 +1,132 @@
+"""
+A Simple BWT script in python that computes BWM, First Occurrences array,
+Checkpoint Array and Suffix Array.
+"""
 
-# global vaiables definition
-bwt = []
-firstOccurrence = []
-checkpointArray = []
-suffixArray = []
+__author__ = "Lorenzo Tabasso"
 
-
-def BWT(sequence):
-    sequence += '$'
-    bwt = [sequence[index:] + sequence[:index]
-           for index, _ in enumerate(sequence)]
-    bwt.sort()
-
-    # transposing the matrix and getting his first row (the first column)
-    for index in range(len(sequence)):
-        firstOccurrence.append(index)
-
-    # Just for test
-    print("your firstOccurrence is: " + repr(firstOccurrence))
-
-    checkpointIndexes = []  # empty list
-    for character in sequence:
-        if character not in checkpointIndexes:
-            checkpointIndexes.append(character)
-    checkpointIndexes.sort()
-
-    # Just for test
-    print("your sorted is: " + repr(checkpointIndexes))
-
-    # Presentation, formatting the string for the video
-
-    print("\n")
-    print("BWT Matrix")
-    length = len(sequence)
-
-    for elem in range(len(sequence)):
-        row = bwt.__getitem__(elem)
-        print('|' + row[0] + ' ' + row[1:length-1] + ' ' + row[length-1] + '| '
-              + repr(firstOccurrence[elem]) + '| ')
-    print("\n")
-
-    finalBwt = [rotation[-1] for rotation in bwt]
-    finalBwt = ''.join(finalBwt)
-
-    return finalBwt
+from itertools import cycle
 
 
-def inverse_BWT(sequence):
-    table = [col for col in sequence]
-    for i in range(len(sequence) - 1):
-        table.sort()
-        table = [sequence[i] + table[i] for i in range(len(sequence))]
-
-    return table[[row[-1] for row in table].index('$')]
+def rotations(t):
+    """ Return list of rotations of input string t """
+    tt = t * 2
+    return [ tt[i:i+len(t)] for i in list(range(0, len(t))) ]
 
 
-sequence = input('Enter sequence: ')
-if sequence != "":
-    bwt = BWT(sequence)
-    print('Burrows-Wheeler Transform:         ' + str(bwt))
+def bwm(t):
+    """ Return lexicographically sorted list of t’s rotations (BWM) """
+    global bwt_all
+    temp = sorted(rotations(t))
 
-    inverse = inverse_BWT(bwt)
-    print('Inverse Burrows-Wheeler Transform: ' + str(inverse))
-else:
-    print("Error: cannot apply BTW on empty string!")
+    for i in list(range(0, len(temp))):
+        bwt_all[i] = (temp[i],)
+
+    return temp
+
+
+def bwt_via_bwm(t):
+    """ Given a string T, returns BWT(T) by way of the BWM """
+    return ''.join(map(lambda x: x[-1], bwm(t)))
+
+
+def first_occurrence():
+    """ Returns the First Occurence array of the input sequence """
+    global bwt_all
+
+    for i in bwt_all:
+        bwt_all[i] += (i,)
+
+
+def checkpoint_array(bwt, b=1):
+    """ Returns the Checkpoint Array of the input sequence """
+
+    global bwt_all, checkpoint_array_letters
+
+    letters = set(bwt)
+    checkpoint_array_letters = sorted(list(letters))
+
+    row = ""
+    for i in bwt_all:
+        if b and i % b == 0:
+            for letter in checkpoint_array_letters:
+                row += str(bwt[:i].count(letter))
+
+            bwt_all[i] += (row,)
+            row = ""
+        else:
+            for letter in checkpoint_array_letters:
+                row += str(bwt[:i].count(letter))
+
+            row = "-" * len(checkpoint_array_letters)
+            bwt_all[i] += (row,)
+            row = ""
+
+
+def suffix_array(a=1):
+    """ Returns the Suffix Array of the input sequence """
+
+    global bwt_all
+
+    for i in bwt_all:
+
+            rotation = bwt_all[i][0]
+            rotation_length = len(rotation)-1
+
+            pool = cycle(rotation)
+            j = 0
+            for c in pool:
+                if c == "$":
+                    break
+                j += 1
+
+            sa = rotation_length - j
+            if a and sa % a == 0:
+                bwt_all[i] += (rotation_length - j,)
+            else:
+                bwt_all[i] += ("--",)
+
+
+def pretty_print(sequence):
+    """ It does some pretty printing """
+
+    str_bwm = "BWM" + " " * int(len(sequence) + 3 - len("BWM"))
+    str_fo = "FO" + " " * 3
+    str_sa = " " * 3 + "SA"
+
+    letter = ""
+    for x in checkpoint_array_letters:
+        letter += x
+
+    print("\n{0}{1}{2}{3}".format(str_bwm, str_fo, letter, str_sa))
+
+    for i in bwt_all:
+        if len(str(bwt_all[i][1])) == 1:
+            print("{0} | {1}  | {2} | {3}".format(bwt_all[i][0], bwt_all[i][1], bwt_all[i][2], bwt_all[i][3]))
+        else:
+            print("{0} | {1} | {2} | {3}".format(bwt_all[i][0], bwt_all[i][1], bwt_all[i][2], bwt_all[i][3]))
+
+
+# Global variables
+bwt_all = {}
+checkpoint_array_letters = []
+
+if __name__ == "__main__":
+    sequence = input('Enter sequence: ')
+    sequence += "$"
+
+    b = input('Enter integer B for the Checkpoint array (blank for 1): ')
+    a = input('Enter integer A for the Suffix array (blank for 1): ')
+    if b == "":
+        b = 1
+    if a == "":
+        a = 1
+
+    final = bwt_via_bwm(sequence)
+
+    first_occurrence()
+    checkpoint_array(final, int(b))
+    suffix_array(int(a))
+    pretty_print(sequence)
+
+    print('\nBurrows-Wheeler Transform:\t{}'.format(final))
